2077662bada5faba7c4ecb7bcdf3f2a6ef6f3245
enesakkose <ens.akkose@gmail.com>
2023-03-29 17:39:06 +0300

    RoundedImg comp. create

---
+++ src/components/RoundedImg/index.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react'
+import Image from 'next/image'
+import styles from '@/components/RoundedImg/RoundedImg.module.scss'
+
+function RoundedImg({
+  width,
+  height,
+  full = false,
+  children,
+}: {
+  width: string | number,
+  height: string | number
+  full?: boolean
+  children: ReactNode
+}) {
+  return (
+    <div
+      className={styles.roundedImg}
+      style={{ width: `${width}px`, height: `${height}px` }}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default RoundedImg
