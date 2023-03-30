169f653d5f560e33381ad40a335a52f757931a7e
enesakkose <ens.akkose@gmail.com>
2023-03-30 17:13:36 +0300

    Accordion menu implementation

--- src/components/AccordionMenu/index.tsx
+++ src/components/AccordionMenu/index.tsx
@@ -1,110 +1,78 @@
 import React, {
-  PropsWithChildren,
   ReactNode,
   useRef,
-  cloneElement,
-  ReactElement,
-  forwardRef,
   useState,
-  Children,
 } from 'react'
 import clsx from 'clsx'
 import ChevronBtn from '@/components/IconButtons/ChevronBtn'
 import styles from '@/components/AccordionMenu/AccordionMenu.module.scss'
 
 type PropsType = {
-  children: ReactNode
+  children: ReactNode | ReactNode[]
   className?: string
 }
 
-export const AccordionHeader = ({ children, className }: PropsType) => {
-  return (
-    <div className={clsx(styles.accordionHeader, className)}>
-      {children}
-      <ChevronBtn />
-    </div>
-  )
+interface PropsWithDataStateType extends PropsType {
+  open?: boolean
+  handleOpen?: () => void
 }
 
-export const AccordionItem = ({ children, className }: PropsType) => {
-  return <div className={clsx(styles.accordionItem, className)}>{children}</div>
-}
-
-export const AccordionContent = ({ children, className }: PropsType) => {
-  return (
-    <div className={clsx(styles.accordionContent, className)}>{children}</div>
-  )
-}
-
-export const AccordionContainer = ({ children, className }: PropsType) => {
+export const Accordion = ({ children, className }: PropsType) => {
   return (
     <div className={clsx(styles.accordionContainer, className)}>{children}</div>
   )
 }
 
-/*
-type PropsType = {
-  children: ReactNode
-  className?: string
+Accordion.Header = ({
+  children,
+  className,
+  handleOpen,
+  open,
+}: PropsWithDataStateType) => {
+
+  return (
+    <div
+      data-state={open ? 'open' : 'closed'}
+      className={clsx(styles.accordionHeader, className)}
+    >
+      {children}
+      <ChevronBtn className={styles.chevronBtn} onClick={handleOpen} />
+    </div>
+  )
 }
 
-interface AccordionChildProps {
-  setOpen: React.Dispatch<React.SetStateAction<boolean>> | undefined;
-  open?: boolean
-}
-
-export const AccordionHeader = forwardRef<HTMLDivElement, PropsType & AccordionChildProps>(({ children, className, setOpen, open }, ref) => {
-  //take to collopse statemnt and change
-  const collapseHandle = () => {
-    setOpen(!open)
+Accordion.Item = ({ children, className }: PropsType) => {
+  const [open, setOpen] = useState(true)
+  const handleOpen = () => {
+    setOpen((prev) => !prev)
   }
 
   return (
-    <div className={clsx(styles.accordionHeader, className)}>
-      {children}
-      <Button onClick={collapseHandle}>
-        <Icon icon='chevron' size={20} />
-      </Button>
-    </div>
-  )
-})
-
-export const AccordionItem = forwardRef<HTMLDivElement, PropsType & AccordionChildProps>(({ children, className, setOpen, open}, ref) => {
-
-  //console.log(open)
-  //take open close statemnt and collopse or no collapse
-  return (
-    <>
-    {open && <div ref={ref} className={clsx(styles.accordionItem, className)}>{children}</div>}
-    </>
-  )
-})
-
-export const AccordionContent = ({ children, className }: PropsType & AccordionChildProps) => {
-  const contentRef = useRef<HTMLDivElement>(null)
-  const [ open, setOpen ] = useState<boolean>(true)
-  //send open close statement to all children
-  const childrenWithRef = React.Children.map(children, child => {
-    if (React.isValidElement(child)) {
-      return React.cloneElement(child, {  setOpen, open })
-    }
-    return child
-  })
-  return (
-    <div ref={contentRef}  className={clsx(styles.accordionContent, className)}>
-      {childrenWithRef}
+    <div className={clsx(styles.accordionItem, className)}>
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child as JSX.Element, { open, handleOpen })
+        }
+        return child
+      })}
     </div>
   )
 }
 
-export const AccordionContainer = ({ children, className }: PropsType) => {
+Accordion.Content = ({ children, className, open }: PropsWithDataStateType) => {
+  const contentRef = useRef<HTMLDivElement | null>(null)
+  const animationHeight = contentRef?.current?.clientHeight
+
   return (
-    <div className={clsx(styles.accordionContainer, className)}>
+    <div
+      data-state={open ? 'open' : 'closed'}
+      style={
+        {'--h': animationHeight && `${animationHeight}px`} as React.CSSProperties
+      }
+      ref={contentRef}
+      className={clsx(styles.accordionContent, className)}
+    >
       {children}
     </div>
   )
 }
-
-
-
-*/
--- src/components/Events/Content.tsx
+++ src/components/Events/Content.tsx
@@ -4,43 +4,41 @@ import { useQuery } from '@tanstack/react-query'
 import EventRow from '@/components/Events/Row'
 import TournamentHeader from '@/components/Events/Header'
 import styles from '@/components/Events/Content.module.scss'
-import {
-  AccordionHeader,
-  AccordionItem,
-  AccordionContainer,
-  AccordionContent,
-} from '../AccordionMenu'
+import { Accordion } from '../AccordionMenu'
 import { getEvents } from '@/utils/hooks'
 
 function Content({ id = 1 }: { id: number }) {
-  const { data, isLoading, error } = useQuery(['events', id], () => getEvents(id))
+  const { data, isLoading, error } = useQuery(['events', id], () =>
+    getEvents(id)
+  )
 
   if (isLoading) return <>'Loading'</>
 
   return (
     <>
       {data?.map((tournament) => (
-        <AccordionContainer
+        <Accordion
           key={tournament.TOURNAMENT_ID}
           className={styles.tournamentEvents}
         >
-          <AccordionItem>
-            <AccordionHeader className={styles.accordionHeader}>
+          <Accordion.Item>
+            <Accordion.Header className={styles.accordionHeader}>
               <TournamentHeader
                 tournamentImage={tournament.TOURNAMENT_IMAGE}
                 tournamentName={tournament.NAME}
               />
-            </AccordionHeader>
-            {tournament.EVENTS.map((match) => (
-              <AccordionContent key={match.EVENT_ID}>
+            </Accordion.Header>
+            <Accordion.Content>
+              {tournament.EVENTS.map((match) => (
                 <EventRow
+                  key={match.EVENT_ID}
                   href={`/event/${match.EVENT_ID}`}
                   event={match}
                 />
-              </AccordionContent>
-            ))}
-          </AccordionItem>
-        </AccordionContainer>
+              ))}
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
       ))}
     </>
   )
