6f0a11d91bb214e1e72741292744163263b9c2e3
enesakkose <ens.akkose@gmail.com>
2023-04-03 23:57:43 +0300

    autoprefixer add

--- next.config.js
+++ next.config.js
@@ -1,20 +1,31 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const postcss = require('postcss')
 
 
 const nextConfig = {
+  swcMinify: true,
+
   experimental: {
     appDir: true,
   },
-
+  //image base url
   images: {
     domains: ['www.flashscore.com'],
   },
-
+  //include scss variables each scss file
   sassOptions: {
     includePaths: [path.join(__dirname)],
     prependData: `@import "@/styles/_variables.scss";`
   },
+
+  //includes autoprixer
+  postcssLoaderOptions: {
+    implementation: postcss,
+    postcssOptions: {
+      plugins: ['autoprefixer']
+    }
+  }
 }
 
 module.exports = nextConfig
--- src/components/Header/ActionBtns/index.tsx
+++ src/components/Header/ActionBtns/index.tsx
@@ -5,10 +5,14 @@ import styles from '@/components/Header/ActionBtns/ActionBtns.module.scss'
 function ActionBtns() {
   return (
     <div className={styles.actionBtns}>
-      <Button icon="star" variant='icon' />
+      <Button icon="star" variant='icon' style={{ gap: '.5rem' }}>
+        FAVORITES
+      </Button>
       <Button icon="gear" variant='icon' />
     </div>
   )
 }
 
 export default ActionBtns
+
+//buttona iconlu style bak dkya ayarla brief logolarını ayarla
