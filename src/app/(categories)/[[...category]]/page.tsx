2a56c1619356d8deb519ba8851c7c575944f0344
enesakkose <ens.akkose@gmail.com>
2023-04-07 01:56:48 +0300

    comps edit

---
+++ src/app/(categories)/[[...category]]/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import TournamentContent from '@/components/Events/Content'
+
+type CategoryRouteType = {
+  params: {
+    category: string
+  }
+}
+
+type SportNameType = {
+  [key: string]: number
+}
+
+const SportName: SportNameType = {
+  football: 1,
+  tennis: 2,
+  basketball: 3,
+}
+
+function Page({ params }: CategoryRouteType) {
+  const id = SportName[params.category] ?? 1
+
+  return <TournamentContent id={id} />
+}
+
+export default Page
