f14c1dea12fa070f9a1f73618bfd6e3d317cc56f
enesakkose <ens.akkose@gmail.com>
2023-04-06 18:47:57 +0300

    use client removed

--- src/app/(categories)/layout.tsx
+++ src/app/(categories)/layout.tsx
@@ -1,9 +1,7 @@
-'use client'
 import React, { ReactNode } from 'react'
 import Header from '@/components/Header'
 import SportsMenu from '@/components/SportsMenu'
 import MainContainer from '@/containers/MainContainer'
-import Sidebar from '@/components/Sidebar'
 import styles from '@/app/(categories)/page.module.scss'
 
 function CategoriesLayout({ children }: { children: ReactNode }) {
