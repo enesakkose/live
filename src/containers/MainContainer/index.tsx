84315cb5ed6f03460cdca5d3a97a661d66d1bc9d
enesakkose <ens.akkose@gmail.com>
2023-04-06 18:36:18 +0300

    location change

--- src/containers/MainContainer/index.tsx
+++ src/containers/MainContainer/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react'
 import clsx from 'clsx'
-import styles from '@/components/MainContainer/MainContainer.module.scss'
+import styles from './MainContainer.module.scss'
 
 function MainContainer({ children, className }: { children: ReactNode, className?: string }) {
   return <div className={clsx(styles.container, className)}>{children}</div>
