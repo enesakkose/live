ad8ab224f1ef729f1c01e94c0af03d1e7e27abba
enesakkose <ens.akkose@gmail.com>
2023-04-07 02:04:21 +0300

    typo

--- src/app/(categories)/layout.tsx
+++ src/app/(categories)/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 import Header from '@/components/Header'
 import SportsMenu from '@/components/SportsMenu'
 import MainContainer from '@/containers/MainContainer'
-import styles from './layout.module.scss'
+import styles from './l.module.scss'
 
 function CategoriesLayout({ children }: { children: ReactNode }) {
   return (
