f9aa4a4d8d1aaa2daf46eb1f7d543e237f615ddd
enesakkose <ens.akkose@gmail.com>
2023-05-04 21:08:57 +0300

    BrandLogo create

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import Search from '@/components/Header/Search'
-import ActionBtns from '@/components/Header/ActionBtns'
+import Search from '../Header/Search'
+import ActionBtns from '../Header/ActionBtns'
 import MainContainer from '@/containers/MainContainer'
-import styles from '@/components/Header/Header.module.scss'
+import BrandLogo from '../BrandLogo'
+import styles from './Header.module.scss'
 
 function Header() {
   return (
     <header className={styles.header}>
       <MainContainer className={styles.container}>
-        <h2>SScore</h2>
+        <BrandLogo/>
         <Search />
         <ActionBtns />
       </MainContainer>
