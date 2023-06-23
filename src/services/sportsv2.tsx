e99c67e8ee82348691ad8c2464ac3d3c77ab654e
enesakkose <ens.akkose@gmail.com>
2023-06-24 02:37:43 +0300

    layout background change and events array edit

--- src/services/sportsv2.tsx
+++ src/services/sportsv2.tsx
@@ -2,33 +2,53 @@ import { useQuery } from '@tanstack/react-query'
 import { getDateV2 } from '@/utils/helpers/getFormatTime'
 import { type Events } from '@/types/Events'
 import { type SearchResult } from '@/types/SearchQuery'
+import { type Statistics } from '@/types/Statistics'
 import { TIMEZONE, type TimezoneStatusType } from '@/context/TimezoneContext'
 
-export const getEventsV2 = async(category: string, date: string) => {
-  const res = await (fetch(`/api/getEvents?category=${category}&date=${date}`))
+export const getEventsV2 = async (category: string, date: string) => {
+  const res = await fetch(`/api/getEvents?category=${category}&date=${date}`)
   const data: Events = await res.json()
-  const currentDateEvents = data.events.filter(event => getDateV2(event.startTimestamp) === date)
-  const liveEventsFromPastDays = data.events.filter(event => event.status.type === 'inprogress')
+  const currentDateEvents = data.events.filter((event) => getDateV2(event.startTimestamp) === date)
+  const liveEventsFromPastDays = data.events.filter(
+    (event) => event.status.type === 'inprogress' && getDateV2(event.startTimestamp) !== date
+  )
+  // DESCRIPTION: liveEventsFromPastDays CREATED FOR IF THERE LIVE EVENTS STARTING BEFORE 00:00 AT PREVIOUS DAY
+
   return [...liveEventsFromPastDays, ...currentDateEvents]
 }
 
-export const getLiveEventsV2 = async(category: string) => {
-  const res = await (fetch(`/api/getLiveEvents?category=${category}`))
+export const getLiveEventsV2 = async (category: string) => {
+  const res = await fetch(`/api/getLiveEvents?category=${category}`)
   const data: Events = await res.json()
   return data.events
 }
 
-export const getSearchResult = async(query: string) => {
-  const res = await (fetch(`/api/getSearchData?q=${query}`))
+export const getSearchResult = async (query: string) => {
+  const res = await fetch(`/api/getSearchData?q=${query}`)
   const data: SearchResult = await res.json()
   return data.results
 }
 
-export const useGetEvents = (category: string, timezoneStatus: TimezoneStatusType, date: string) => {
-  return useQuery(['eventsV2', category, timezoneStatus, date], 
-    () => timezoneStatus === TIMEZONE.LIVE ? getLiveEventsV2(category) : getEventsV2(category, date))
+export const getEventStats = async(eventId: number) => {
+  const res = await fetch(`/api/statistics?eventId=${eventId}`)
+  const data: Statistics = await res.json()
+  return data.statistics
+}
+
+export const useGetEvents = (
+  category: string,
+  timezoneStatus: TimezoneStatusType,
+  date: string
+) => {
+  return useQuery(['eventsV2', category, timezoneStatus, date], () =>
+    timezoneStatus === TIMEZONE.LIVE ? getLiveEventsV2(category) : getEventsV2(category, date)
+  )
 }
 
 export const useGetSearchResult = (query: string) => {
   return useQuery([query, 0], () => getSearchResult(query))
+}
+
+export const useGetEventStats = (eventId: number) => {
+  return useQuery(['statistics', eventId], () => getEventStats(eventId))
 }
