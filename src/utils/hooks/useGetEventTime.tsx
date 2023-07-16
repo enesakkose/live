fb04024709aa8a979bad70ae659a64e2d5b30fcd
enesakkose <ens.akkose@gmail.com>
2023-07-17 00:29:34 +0300

    fix

--- src/utils/hooks/useGetEventTime.tsx
+++ src/utils/hooks/useGetEventTime.tsx
@@ -4,23 +4,25 @@ import { EVENT_STATUS } from '@/types/Events'
 export const useGetEventTime = (startTime: number, status: string) => {
   
   const getElapsedMinutes = () => {
-    const currentTimestamp = Math.floor(Date.now() / 1000)
-    let startTimestamp
-    
-    if (status === EVENT_STATUS.FIRST_HALF) {
-      startTimestamp = 1
-    } else if(status === EVENT_STATUS.FIRST_EXTRA) {
-      startTimestamp = 91 * 60
-    } else if(status === EVENT_STATUS.SECOND_EXTRA) {
-      startTimestamp = 106 * 60
-    } else{
-      startTimestamp = 46 * 60
+    return () => {
+      const currentTimestamp = Math.floor(Date.now() / 1000)
+
+      let startTimestamp
+      if (status === EVENT_STATUS.FIRST_HALF) {
+        startTimestamp = 1
+      } else if(status === EVENT_STATUS.FIRST_EXTRA) {
+        startTimestamp = 91 * 60
+      } else if(status === EVENT_STATUS.SECOND_EXTRA) {
+        startTimestamp = 106 * 60
+      } else{
+        startTimestamp = 46 * 60
+      }
+
+      const elapsedTime = currentTimestamp - startTime
+      const elapsedMinutes = Math.floor((startTimestamp + elapsedTime) / 60)
+
+      return elapsedMinutes
     }
-    
-    const elapsedTime = currentTimestamp - startTime
-    const elapsedMinutes = Math.floor((startTimestamp + elapsedTime) / 60)
-    
-    return elapsedMinutes
   }
 
   const elapsedMinutes = getElapsedMinutes()
