93266b4b6a02fc6436a18bcff97f223b2aba6269
enesakkose <ens.akkose@gmail.com>
2023-07-20 00:19:09 +0300

    ScrolContainer component made generic

--- src/containers/ScrollContainer/index.tsx
+++ src/containers/ScrollContainer/index.tsx
@@ -2,20 +2,21 @@ import React from 'react'
 import clsx from 'clsx'
 import styles from './ScrollContainer.module.scss'
 
-type ScrollContainerPropsType = {
+type ScrollContainerPropsType<T extends React.ElementType = 'div'> = {
   children: React.ReactNode
   className?: string
-  as?: keyof JSX.IntrinsicElements
-}
+  as?: T
+} & React.ComponentPropsWithoutRef<T>
 
-function ScrollContainer({ children, as = 'div', className }: ScrollContainerPropsType) {
-  const Element = as
+const ScrollContainer = <T extends React.ElementType = 'div'>({
+  children,
+  as,
+  className,
+  ...props
+}: ScrollContainerPropsType<T>) => {
+  const Element = as || 'div'
 
-  return (
-    <Element className={clsx(styles.scrollContainer, className)}>
-      {children}
-    </Element>
-  )
+  return <Element className={clsx(styles.scrollContainer, className)} {...props}>{children}</Element>
 }
 
 export default ScrollContainer
