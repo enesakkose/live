3681434ba6ddc3a98cd8be36f42ecd4cf7d8c289
enesakkose <ens.akkose@gmail.com>
2023-07-20 20:04:07 +0300

    Timezone display fix

--- src/app/(categories)/layout.tsx
+++ src/app/(categories)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '@/components/Header'
 import MainContainer from '@/containers/MainContainer'
 import BottomNav from '@/components/BottomNav'
+import Timezone from '@/components/Events/Timezone'
 import styles from './layout.module.scss'
 
 function CategoriesLayout({ children }: { children: React.ReactNode }) {
@@ -9,6 +10,7 @@ function CategoriesLayout({ children }: { children: React.ReactNode }) {
     <MainContainer className={styles.container}>
       <Header />
       <main className={styles.sportTimezone}>
+        <Timezone />
         {children}
         <BottomNav />
       </main>
